8670b82d0020a1c4df0b752c104e5f8ecd42c9be
Rick SanChez <madrjck77@gmail.com>
2022-11-14 16:33:41 +0400

    Add project files.

---
+++ SwordDamageApp/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamageApp
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultipliyer = 0;
+        public int FLamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultipliyer) + BASE_DAMAGE + FLamingDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultipliyer = 1.75M;
+            }
+            else
+            {
+                MagicMultipliyer = 1M;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+
+    }
+}
---
+++ SwordDamageApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwordDamageApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage.SetMagic(false);
+            swordDamage.SetFlaming(false);
+            RollDice();
+        }
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.SetFlaming(flaming.IsChecked.Value);
+            swordDamage.SetFlaming(magic.IsChecked.Value);
+            DisplayDamage();
+        }
+        void DisplayDamage ()
+        {
+            damage.Text = "Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP";
+        }
+       
+        private void flaming_Checked ( object sender, RoutedEventArgs e )
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage(); 
+        }
+
+        private void flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+
+        private void magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
